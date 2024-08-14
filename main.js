862a7b70c9e36c202d5cc35245da8a6070cfcc66
tranthanhhthao <90049000+tranthanhhthao@users.noreply.github.com>
2024-08-14 22:43:12 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,53 @@ tl
         duration: .6,
         opacity: 1, 
     }, "+=1")
+
+// preloader
+const buttonContact = document.getElementById('click-contact');
+const buttonProjects = document.getElementById('click-projects');
+
+const preloaderTl = gsap.timeline({ defaults: { ease: "power1.out", duration: 0.4 } });
+
+function delayToLink(URL) {
+    setTimeout(() => {
+        window.location.href = URL;
+    }, 3000)
+}
+
+buttonContact.addEventListener('click', () => {
+    gsap.set('.preload', {
+        y: '-100%',
+        backgroundColor: getComputedStyle(document.body).getPropertyValue('--yellow-first'),
+    })
+
+    preloaderTl
+        .to('.preload', {
+            y: '0%',
+            duration: 2,
+            display: 'block',
+            ease: 'elastic.out(1, 0.5)',
+            delay: 0.3,
+        })
+
+    delayToLink('https://tranthanhhthao.github.io/swinburne-contact/')
+
+});
+
+// buttonProjects.addEventListener('click', () => {
+//     gsap.set('.preload', {
+//         y: '100%',
+//         backgroundColor: getComputedStyle(document.body).getPropertyValue('--green-first'),
+//     })
+
+//     preloaderTl
+//         .to('.preload', {
+//             y: '0%',
+//             duration: 2,
+//             display: 'block',
+//             ease: 'elastic.out(1, 0.5)',
+//             delay: 0.3,
+//         })
+    
+
+// });    
+
